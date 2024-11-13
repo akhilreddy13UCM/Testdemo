43729dd3a125bcdc25e5a465e5833187322c5877
akhilreddy13UCM <axe55580@ucmo.edu>
2024-11-13 15:46:18 -0600

    Before,After methods

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,19 +1,35 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@Slf4j
 class DemoApplicationTests {
-
-	@Test
-	@Disabled
-	void contextLoads() {
+	@BeforeEach
+	void setUp()
+	{
+		log.info("run before each test case");
 	}
-
+	@AfterEach
+	void tearDown()
+	{
+		log.info("tearing down method,run after each test case");
+	}
+	@BeforeAll
+	static void setUpOnce()
+	{
+		log.info("run before all test case");
+	}
+	@AfterAll
+	static void tearDownAll()
+	{
+		log.info("tearing down method,run all each test case");
+	}
 	@Test
+	@DisplayName("Test")
 	void Test1() {
+		log.info("test1 is run");
 	}
 
 }
